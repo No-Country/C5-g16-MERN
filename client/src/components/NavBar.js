836b338396a417ecc0c5ccfbf3ca285a3b4b103d
Lino Cruz <linoeduardocd@gmail.com>
2022-05-27 07:45:49 -0500

    Nav bar updated

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,28 +1,37 @@
+import Logo from "../Images/field.svg";
+import "./../Styles/navbar.css";
 
-import Logo from  "../Images/field.svg"
-import './../Styles/navbar.css'
+const NavBar = () => {
+  return (
+    <div className="nav">
+      <div>
+        <h1 className="title">La Cancha</h1>
+        {/* <a href="home" className="" ><img src= {Logo} alt = "#"/></a>  */}
+        <span className="subtitle">
+          Canchas cerca para jugar con los amigos
+        </span>
+      </div>
+      <nav>
+        <ul className="navul">
+          <li>
+            <a href="#centro">Perfil</a>
+          </li>
+          <li>
+            <a href="#favoritas">Favoritas</a>
+          </li>
+          <li>
+            <a href="#buscar">Buscar</a>
+          </li>
+          <li>
+            <a href="#registrate">Registrate</a>
+          </li>
+          <li>
+            <a href="#ingressa">Ingresar</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-
-const NavBar = () =>  {
-    return (
-        <div className="nav">
-            <div >
-                <h1 className="title">La Cancha</h1>
-               {/* <a href="home" className="" ><img src= {Logo} alt = "#"/></a>  */}
-                <span className="subtitle">Reservo mi cancha</span>
-            </div>
-        <nav>
-            <ul className="navul">
-                
-                <li><a href="#centro">Perfil</a></li>
-                <li><a href="#favoritas">Favoritas</a></li>
-                <li><a href="#buscar">Buscar</a></li>
-                <li><a href="#registrate">Registrate</a></li>
-                <li><a href="#ingressa">Ingresar</a></li>
-            </ul>
-        </nav>
-        </div>
-    )
-}
-
-export default NavBar 
+export default NavBar;
