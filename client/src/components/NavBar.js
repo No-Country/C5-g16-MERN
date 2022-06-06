5900af6d95249e7365be1a24a6feede686b0c584
AndyCambra <andycambra@gmail.com>
2022-06-06 18:41:11 -0300

    Modal

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,37 @@
 import { Typography, Box } from "@mui/material";
-import { Button } from "@mui/material";
+import { Button, Modal } from "@mui/material";
 import { Link, useParams } from "react-router-dom";
 import "../Styles/navbarStyles.js";
 import useStyles from "../Styles/navbarStyles.js"
+import { useState } from "react";
+import Login from "./Login.js";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  height: 500,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
 
 const NavBar = () => {
   const classes = useStyles()
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
     <div className={classes.nav}>
@@ -19,9 +45,17 @@ const NavBar = () => {
       <nav>
         <ul className={classes.navul}>
           <li>
-            <Link to="/login">
-              <Button variant="contained" size="small" color="success" className={classes.goButton}>Ingresar</Button>
-            </Link>
+          <Button onClick={handleOpen} variant="contained" color="success">Ingresar </Button>
+<Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style} >
+   <Login />
+  </Box>
+</Modal>
           </li>
         </ul>
       </nav>
